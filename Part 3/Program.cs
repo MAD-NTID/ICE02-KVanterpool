308658605371528c849ff27feda70460753de356
Kiarra Vanterpool (RIT Student) <kiv8873@ad.rit.edu>
2019-08-28 10:59:11 -0400

    My first commit on GitHub!

---
+++ Part 3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mad students - I'm Kiarra Vanterpool");
+            Console.WriteLine("I'm from The Virgin Island");
+        }
+    }
+}
